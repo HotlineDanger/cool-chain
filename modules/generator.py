72f5d912b27340d380278c533af1d67e68fc4739
Thibault Smetryns <tsmetryns@corero.com>
2017-08-24 13:46:33 +0100

    progress generator

---
+++ modules/generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# this piece of code is in charge of creatng blocks for our blockchain
+# we create the blockhain before adding the first block - the genesis block
+
+blockchain = [generate_genesis_block()]
+
+
