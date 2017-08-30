a8f05c47accf86303adfb4980adc49ba5ae55e1a
Thibault Smetryns <tsmetryns@corero.com>
2017-08-30 14:07:26 +0100

    handled imports and renamed files accordingly

--- modules/NewBlock.py
+++ modules/NewBlock.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from Block import Block
+from datetime import datetime
 
 def next_block(last_block):
     block_id = last_block.index + 1
-    block_timestamp = date.datetime.now()
+    block_timestamp = datetime.now()
     block_data = "Block " + str(block_id)
     block_hash = last_block.hash
 
